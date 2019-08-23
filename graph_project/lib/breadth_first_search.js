933fcc6a05b683a040ed3948e722966a0ee5435a
wdunn <williamdunn195@gmail.com>
2019-08-23 13:48:19 -0400

    Complete graph problems

--- graph_project/lib/breadth_first_search.js
+++ graph_project/lib/breadth_first_search.js
@@ -1,5 +1,15 @@
-function breadthFirstSearch(startingNode, targetVal) {
+function breadthFirstSearch(startingNode, targetVal, visited = new Set()) {
+    let queue = [ startingNode ]
 
+    while (queue.length) {
+        let node = queue.shift();
+        visited.add(node);
+        if (node.val === targetVal) return node;
+        node.neighbors.forEach((neighbor) => visited.has(neighbor) ? null : queue.push(neighbor) );
+    }
+   
+    return null;
+    
 }
 
 module.exports = {
--- graph_project/lib/max_value.js
+++ graph_project/lib/max_value.js
@@ -1,5 +1,18 @@
-function maxValue(node, visited=new Set()) {
+function maxValue(startNode, visited=new Set()) {
+    let queue = [ startNode ];
+    let max = -Infinity;
 
+    while (queue.length) {
+        let node = queue.shift();
+        if (node.val > max) max = node.val;
+        
+        if (visited.has(node)) continue;
+        visited.add(node);
+
+        queue.push(...node.neighbors);
+    }
+
+    return max;
 }
 
 module.exports = {
